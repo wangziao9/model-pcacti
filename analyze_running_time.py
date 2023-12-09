7b00f50d2850b82cf7f11459415719bee46f8690
wangziao <1575538687@qq.com>
2023-12-08 17:53:13 -0800

    remove basex for compatibility and add error message

--- analyze_running_time.py
+++ analyze_running_time.py
@@ -14,9 +14,15 @@ from train import get_dataframes, transform_frames, split_train_test
 import matplotlib.pyplot as plt
 
 # Analyze running time of cacti
-f_l2 = open("L2_data.csv", 'r')
-csv_title = f_l2.readline().strip().split(',')
-f_l2.close()
+try:
+    f_l2 = open("L2_data.csv", 'r')
+    csv_title = f_l2.readline().strip().split(',')
+    f_l2.close()
+    f_l3 = open("L3_data.csv", 'r')
+    f_l3.close()
+except FileNotFoundError:
+    print("please prepare L2 and L3 data, make sure L2_data.csv, L3_data.csv exists in the current directory")
+    
 size_idx = csv_title.index("cache_size")
 time_idx = csv_title.index("elapsed_time (s)")
 for layer in ["L2", "L3"]:
@@ -42,7 +48,7 @@ for layer in ["L2", "L3"]:
         print(f"cache size:{2**log_size}B (log = {log_size}), average cacti running time: {'%.4f'%avg_running_time}(s)")
     plt.plot([2**size for size in log_sizes], avg_times, 'o', label=layer+" cache")
 plt.xlabel("Cache size (B)")
-plt.xscale('log', basex=2)
+plt.xscale('log') # can add basex=2 here optionally
 plt.ylabel("Running time (s)")
 plt.legend()
 plt.title("Average Running time for P-CACTI")
